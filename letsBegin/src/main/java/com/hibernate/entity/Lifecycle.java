fcdf3c59b2b7a3c1a7da9878c2ca4b2df0425b9b
shubhamsingh <shubhusingh1308@gmail.com>
2024-03-07 00:41:50 +0530

    Created session Factory

--- letsBegin/src/main/java/com/hibernate/entity/Lifecycle.java
+++ letsBegin/src/main/java/com/hibernate/entity/Lifecycle.java
@@ -1,10 +1,13 @@
 package com.hibernate.entity;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Lifecycle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 	}
 
 }
